f7128de26254cab568b873faedc576773e4f707b
Shannon Ernst <ernstsh@flip3.engr.oregonstate.edu>
2016-05-15 14:15:56 -0700

    edited config

---
+++ Config.php
@@ -0,0 +1,8 @@
+<?php
+
+	define('DB_SERVER','mysql.cs.orst.edu');
+	define('DB_USERNAME', 'cs340_ernstsh');
+	define('DB_PASSWORD', 'Fredis14');
+	define('DB_DATABASE', 'cs340_ernstsh');
+	$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
