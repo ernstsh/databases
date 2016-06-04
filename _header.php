559fc1930bb7c0652413996457036dd15b7b8567
jia790 <chenj2@oregonstate.edu>
2016-06-04 16:06:40 -0700

    give it a try, the screen still didn't change on my side

--- _header.php
+++ _header.php
@@ -1,39 +1,5 @@
 <?php
-    session_start();
-	//this is doe checking for login stuff 
-   /*function checkAuth($doRedirect) {
-	if (isset($_SESSION["onidid"]) && $_SESSION["onidid"] != "") return $_SESSION["onidid"];
-
-	 $pageURL = 'http';
-	 if (isset($_SERVER["HTTP"]) && $_SERVER["HTTP"] == "on") {$pageURL .= "s";}
-	 $pageURL .= "://";
-	 if ($_SERVER["SERVER_PORT"] != "80") {
-	  $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["SCRIPT_NAME"];
-	 } else {
-	  $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["SCRIPT_NAME"];
-	 }
-
-	$ticket = isset($_REQUEST["ticket"]) ? $_REQUEST["ticket"] : "";
-
-	if ($ticket != "") {
-		$url = "https://login.oregonstate.edu/cas/serviceValidate?ticket=".$ticket."&service=".$pageURL;
-		$html = file_get_contents($url);
-		$pattern = '/\\<cas\\:user\\>([a-zA-Z0-9]+)\\<\\/cas\\:user\\>/';
-		preg_match($pattern, $html, $matches);
-		if ($matches && count($matches) > 1) {
-			$onidid = $matches[1];
-			$_SESSION["onidid"] = $onidid;
-			return $onidid;
-		} 
-	} else if ($doRedirect) {
-		$url = "https://login.oregonstate.edu/cas/login?service=".$pageURL;
-		//echo $url;
-		echo "<script>location.replace('" . $url . "');</script>";
-	} 
-	return "";
-}*/
-
-    
+    session_start();    
 ?>
 
 <html lang = "en">
