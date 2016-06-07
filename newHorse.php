3aaecc5efe30ab99d641fc4d4d96881ba09c4ac0
jia790 <chenj2@oregonstate.edu>
2016-06-07 15:10:28 -0700

    final one

--- newHorse.php
+++ newHorse.php
@@ -18,7 +18,7 @@ include("_header.php");
 				<input type="text" name="breed" required>
 			<br>
 			<label>Registration</label>
-				<input type="text" pattern = "[a-zA-Z0-9 ]{3,15}" title = "enter numbers & letters !" name="reg">
+				<input type="text" pattern = "[a-zA-Z0-9 ]{3,15}" title = "enter numbers & letters !" name="reg" required>
 			<br>
 			<label>Gender</label><br>
 				<input type="radio" name="gender" value="Stallion">Stallion<br>
@@ -26,55 +26,55 @@ include("_header.php");
 				<input type="radio" name="gender" value="Mare">Mare
 			<br>
 			<label>Age</label>
-				<input type="text" pattern = "[0-9 ]{1,2}" title = "enter numbers !" name="age">
+				<input type="text" pattern = "[0-9 ]{1,2}" title = "enter numbers !" name="age" required>
 			<br>
 			<label>Height</label>
-				<input type="text" pattern = "[0-9. ]+" title = "enter numbers in this format #.#!" name="height">
+				<input type="text" pattern = "[0-9. ]+" title = "enter numbers in this format #.#!" name="height" required>
 			<br>
 			<label>Owner</label>
-				<input type="text" pattern = "[a-zA-Z ]{1,30}" title = "enter letters between 1 to 30!" name="owner">
+				<input type="text" pattern = "[a-zA-Z ]{1,30}" title = "enter letters between 1 to 30!" name="owner" required>
 			<br>
 			<label>Leasee</label>
-				<input type="text" pattern = "[a-zA-Z ]{4,30}" title = "enter in 'none' for now!"name="leasee">
+				<input type="text" pattern = "[a-zA-Z ]{4,30}" title = "enter in 'none' for now!"name="leasee" required>
 			<br>
 			
 			<h3>Vet Information</h3>
 			<label>First Name</label>
-				<input type="text" pattern = "[a-zA-Z ]{1,20}" title = "enter letters 1 to 20!" name="vfirstName"> <br>
+				<input type="text" pattern = "[a-zA-Z ]{1,20}" title = "enter letters 1 to 20!" name="vfirstName" required> <br>
 			<label>Last Name</label>
-				<input type="text" pattern = "[a-zA-Z ]{1,20}" title = "enter letters 1 to 20!" name="vlastName"> <br>
+				<input type="text" pattern = "[a-zA-Z ]{1,20}" title = "enter letters 1 to 20!" name="vlastName" required> <br>
 			<label>Phone</label>
-				<input type="text" pattern = "[0-9- ]{12,13}" title = "enter numbers in this format ###-###-#### !" name="vphone"> <br>
+				<input type="text" pattern = "[0-9- ]{12,13}" title = "enter numbers in this format ###-###-#### !" name="vphone" required> <br>
 			<h3>Vet Office Location</h3>
 			<label>Street Address</label>
-				<input type="text" pattern = "[a-zA-Z0-9 ]{6,30}" title = "enter letters 6 to 20!" name="vstreet"> <br>
+				<input type="text" pattern = "[a-zA-Z0-9 ]{6,30}" title = "enter letters 6 to 20!" name="vstreet" required> <br>
 			<label>City</label>
-				<input type="text" pattern = "[a-zA-Z ]{3,30}" title = "enter letters 3 to 30!" name="vcity"> <br>
+				<input type="text" pattern = "[a-zA-Z ]{3,30}" title = "enter letters 3 to 30!" name="vcity" required> <br>
 			<label>State</label>
-				<input type="text" pattern = "[a-zA-Z]{2}" title = "enter 2 letters !" name="vstate"> <br>
+				<input type="text" pattern = "[a-zA-Z]{2}" title = "enter 2 letters !" name="vstate" required> <br>
 			<label>Zip Code</label>
-				<input type="text" pattern = "[0-9]{6}" title = "enter 6 numbers" name="vzip"> <br>
+				<input type="text" pattern = "[0-9]{6}" title = "enter 6 numbers" name="vzip" required> <br>
 				
 			<h3>Farrier Information</h3>
 			<label>First Name</label>
-				<input type="text" pattern = "[a-zA-Z ]{1,20}" title = "enter letters 1 to 20!" name="ffirstName"> <br>
+				<input type="text" pattern = "[a-zA-Z ]{1,20}" title = "enter letters 1 to 20!" name="ffirstName" required> <br>
 			<label>Last Name</label>
-				<input type="text" pattern = "[a-zA-Z ]{1,20}" title = "enter letters 1 to 20!" name="flastName"> <br>
+				<input type="text" pattern = "[a-zA-Z ]{1,20}" title = "enter letters 1 to 20!" name="flastName" required> <br>
 			<label>Phone</label>
-				<input type="text" pattern = "[0-9- ]{12,13}" title = "enter numbers in this format ###-###-#### !" name="fphone"> <br>
+				<input type="text" pattern = "[0-9- ]{12,13}" title = "enter numbers in this format ###-###-#### !" name="fphone" required> <br>
 			<h3>Farrier Mailing Address</h3>
 			<label>Street Address</label> <br>
-				<input type="text" pattern = "[a-zA-Z0-9 ]{6,30}" title = "enter letters 6 to 20!" name="fstreet"> <br>
+				<input type="text" pattern = "[a-zA-Z0-9 ]{6,30}" title = "enter letters 6 to 20!" name="fstreet" required> <br>
 			<label>City</label> 
-				<input type="text" pattern = "[a-zA-Z ]{3,30}" title = "enter letters 3 to 30!" name="fcity"> <br>
+				<input type="text" pattern = "[a-zA-Z ]{3,30}" title = "enter letters 3 to 30!" name="fcity" required> <br>
 			<label>State</label>
-				<input type="text" pattern = "[a-zA-Z]{2}" title = "enter 2 letters !" name="fstate"> <br>
+				<input type="text" pattern = "[a-zA-Z]{2}" title = "enter 2 letters !" name="fstate" required> <br>
 			<label>Zip Code</label>
-				<input type="text" pattern = "[0-9]{6}" title = "enter 6 numbers" name="fzip"> <br>
+				<input type="text" pattern = "[0-9]{5}" title = "enter 6 numbers" name="fzip" required> <br>
 				
 			<h3>Training Program</h3>
 			<label>Number of Days Per Week</label>
-				<input type="text" pattern = "[0-9]{1,7}" title = "enter a number 1 to 7" name="dayPW"> <br>
+				<input type="text" pattern = "[0-9]{1,7}" title = "enter a number 1 to 7" name="dayPW" required> <br>
 			<label>Disciplines (Check all that apply)</label> <br>
 				<input type="checkbox" name="English" value="1">English
 				<input type="checkbox" name="Western" value="1">Western
